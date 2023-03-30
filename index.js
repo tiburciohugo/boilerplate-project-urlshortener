1696db0dd95bd01a082b7f10ade7e0581b9d6af0
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-03-30 13:14:54 -0300

    New method without DB

--- index.js
+++ index.js
@@ -3,18 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dns = require("dns");
-const urlparser = require("url");
-const mongoose = require("mongoose");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-mongoose.connect(process.env.DB_URI, {
-  useNewURLParser: true,
-  useUnifiedtopology: true,
-});
-
-const schema = new mongoose.Schema({ url: "string" });
-const Url = mongoose.model("Url", schema);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -30,22 +21,37 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+const urls = [];
+const shortUrls = [];
+
+function isValidUrl(url) {
+  const regex = /^https?:\/\//;
+  const urlWithoutProtocol = url.replace(regex, "").replace(/\/$/, "");
+  return new Promise((resolve, reject) => {
+    dns.lookup(urlWithoutProtocol, (err, address, family) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
+
 // URL Shortener
 app.get("/api/shorturl/:shorturl", async (req, res, next) => {
-  try {
-    const { short_url } = req.params;
-    const url = await Url.findOne({ short_url });
-    if (!url) {
-      res.json({ error: "Invalid URL" });
-    } else {
-      res.redirect(url.url);
-    }
-  } catch (error) {
-    next(error);
+  const shortUrl = parseInt(req.params.shorturl);
+  const originalUrlIndex = shortUrls.indexOf(shortUrl);
+
+  if (originalUrlIndex >= 0) {
+    const originalUrl = urls[originalUrlIndex];
+    res.redirect(originalUrl);
+  } else {
+    res.status(404).json({ error: "Short URL not found" });
   }
 });
 
-app.post("/api/shorturl", async function (req, res) {
+app.post("/api/shorturl", async (req, res) => {
   const { url } = req.body;
 
   if (!url || url.trim() === "") {
@@ -53,12 +59,13 @@ app.post("/api/shorturl", async function (req, res) {
   }
 
   try {
-    const { hostname } = new URL(url);
-    await dns.promises.lookup(hostname);
-    const shortUrl = new Url({ url });
-    const savedUrl = await shortUrl.save();
-    res.json({ original_url: url, short_url: savedUrl._id });
-  } catch (error) {
+    await isValidUrl(url);
+    const shortUrl = shortUrls.length + 1;
+    urls.push(url);
+    shortUrls.push(shortUrl);
+    console.log(urls, shortUrls);
+    res.json({ original_url: url, short_url: shortUrl });
+  } catch (err) {
     res.json({ error: "invalid URL" });
   }
 });
