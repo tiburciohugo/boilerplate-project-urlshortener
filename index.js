5244481e9f46e779979f41c8e15f1fb38f1b7601
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-03-30 12:25:48 -0300

    Adding DB + Endpoints modifications

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dns = require("dns");
+const urlparser = require("url");
+const mongoose = require("mongoose");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose.connect(process.env.DB_URI, {
+  useNewURLParser: true,
+  useUnifiedtopology: true,
+});
+
+const schema = new mongoose.Schema({ url: "string" });
+const Url = mongoose.model("Url", schema);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -21,43 +30,37 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-const urls = [];
-const shortUrls = [];
-
 // URL Shortener
-app.get("/api/shorturl/:shorturl", (req, res, next) => {
-  const shortUrl = parseInt(req.params.shorturl);
-  const originalUrlIndex = shortUrls.indexOf(shortUrl);
-
-  if (originalUrlIndex >= 0) {
-    const originalUrl = urls[originalUrlIndex];
-    res.redirect(originalUrl);
-  } else {
-    res.status(404).json({ error: "Short URL not found" });
+app.get("/api/shorturl/:shorturl", async (req, res, next) => {
+  try {
+    const { short_url } = req.params;
+    const url = await Url.findOne({ short_url });
+    if (!url) {
+      res.json({ error: "Invalid URL" });
+    } else {
+      res.redirect(url.url);
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
-app.post("/api/shorturl", function (req, res) {
+app.post("/api/shorturl", async function (req, res) {
   const { url } = req.body;
 
   if (!url || url.trim() === "") {
     return res.status(400).json({ error: "a URL is required" });
   }
 
-  const regex = /^https?:\/\//;
-  const urlWithoutProtocol = url.replace(regex, "").replace(/\/$/, "");
-
-  dns.lookup(urlWithoutProtocol, function (err, address, family) {
-    if (err) {
-      res.json({ error: "invalid URL" });
-    } else {
-      const shortUrl = shortUrls.length + 1;
-      urls.push(url);
-      shortUrls.push(shortUrl);
-      console.log(urls, shortUrls);
-      res.json({ original_url: url, short_url: shortUrl });
-    }
-  });
+  try {
+    const { hostname } = new URL(url);
+    await dns.promises.lookup(hostname);
+    const shortUrl = new Url({ url });
+    const savedUrl = await shortUrl.save();
+    res.json({ original_url: url, short_url: savedUrl._id });
+  } catch (error) {
+    res.json({ error: "invalid URL" });
+  }
 });
 
 app.listen(port, function () {
